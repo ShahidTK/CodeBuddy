084b2a3ed971fb9d5c1176ed6e98995da70580c4
Shahid <shahidvelom99@gmail.com>
2025-04-06 17:20:41 +0530

    Add struct setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
 function App() {
 
+  const showToasts = () => {
+    toast.success("Authentication successful!", {
+      autoClose: 5000, 
+      closeOnClick:"true"
+    });
+  };
+
   return (
     <>
-      <h1>Hello world</h1>
+      <ToastContainer position="bottom-center"  hideProgressBar="false"/>
+      <h1 className="text-3xl bg-green-100 p-2 m-2">
+      
+        Hello Shahid!
+      </h1>
+      <button
+        onClick={showToasts}
+        className="m-2 p-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Show Toasts
+      </button>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;

8ae617dac306f18157362e09469cff040427e7ee
Shahid <shahidvelom99@gmail.com>
2025-04-09 19:53:20 +0530

    add signup logic

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-// console.log('Mongo URI:', process.env.MONGODB_URL);
+console.log('Mongo URI:', process.env.MONGODB_URL);
 
 
 export const connectDB= async () => {
     try{
-        const conn=await mongoose.connect(process.env.MONGODB_URL);
+        const conn=await mongoose.connect(`mongodb+srv://shahidvelom99:shahid2001@cluster0.ni4eci5.mongodb.net/chat-app?retryWrites=true&w=majority&appName=Cluster0`);
         console.log(`mongoDB connected: ${conn.connection.host}`);
     } catch(error){
         console.error(`MongoDB connection error: ${error.message}`);

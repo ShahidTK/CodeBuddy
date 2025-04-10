a969e9c41402b9c00dd4e811a441a61c953d2fdd
Shahid <shahidvelom99@gmail.com>
2025-04-10 06:22:47 +0530

    Fix mongoDB connection bug

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -7,7 +7,7 @@ console.log('Mongo URI:', process.env.MONGODB_URL);
 
 export const connectDB= async () => {
     try{
-        const conn=await mongoose.connect(`mongodb+srv://shahidvelom99:shahid2001@cluster0.ni4eci5.mongodb.net/chat-app?retryWrites=true&w=majority&appName=Cluster0`);
+        const conn=await mongoose.connect(process.env.MONGODB_URL);
         console.log(`mongoDB connected: ${conn.connection.host}`);
     } catch(error){
         console.error(`MongoDB connection error: ${error.message}`);

88c823b6a93b3d38fc26622d2aa5a3e83cf7ca91
Shahid <shahidvelom99@gmail.com>
2025-04-11 14:45:43 +0530

    add sign up and log in front end design

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,30 +1,47 @@
-import { useState } from 'react';
+import { use, useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
+import Navbar from './components/Navbar.jsx';
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import SettingsPage from './pages/SettingsPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+
+import {Routes, Route, Navigate} from "react-router-dom";
+import { useAuthStore } from './store/useAuthStore.js';
+import { Loader } from "lucide-react"
+
+
+
 
 function App() {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
 
-  const showToasts = () => {
-    toast.success("Authentication successful!", {
-      autoClose: 5000, 
-      closeOnClick:"true"
-    });
-  };
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth]);
+  console.log({authUser});
+
+  // for loading icon
+  if(isCheckingAuth && !authUser) return (
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10 animate-spin'/>
+    </div>
+  )
 
   return (
     <>
       <ToastContainer position="bottom-center"  hideProgressBar="false"/>
-      <h1 className="text-3xl bg-green-100 p-2 m-2">
-      
-        Hello Shahid!
-      </h1>
-      <button
-        onClick={showToasts}
-        className="m-2 p-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Show Toasts
-      </button>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={authUser?<HomePage/>: <Navigate to="/login" />} />
+        <Route path='/signup' element={!authUser? <SignUpPage/>: <Navigate to="/" />} />
+        <Route path='/login' element={!authUser? <LoginPage/>: <Navigate to="/" />} />
+        <Route path='/settings' element={<SettingsPage/>} />
+        <Route path='/profile' element={authUser? <ProfilePage/>: <Navigate to="/login" />} />
+      </Routes>
+
       
     </>
   );
---
+++ front-end/src/pages/ProfilePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfilePage = () => {
+const {authUser} = useAuthStore()
+    
+  return (
+    <div>
+    Profile page  
+    </div>
+  )
+}
+
+export default ProfilePage
